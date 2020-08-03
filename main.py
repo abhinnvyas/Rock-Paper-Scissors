a5e964799dc248bd749a5314ca05b7cce4625c6b
Abhinn Vyas <abhinnvyas2@gmail.com>
2020-08-03 09:21:40 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import time
+import os
+
+game = ['rock','paper','scissors']
+
+while True:
+    os.system('cls')
+    print("Start getting you hands in Motion!!!")
+    time.sleep(1)
+    temp = game[random.randrange(0,2)]
+
+    for i in game:
+        print(i,"!")
+        time.sleep(0.5)
+
+    print(f">>>{temp}")
+
+    userInput= input("\nDo you want to play another game?")
+
+    if userInput.lower() == "y":
+        continue
+    if userInput.lower() == "n":
+        exit()
+    else:
+        continue
